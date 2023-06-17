f31675579dbb09c1fe7bf1ff65a43f4a3c23b1c7
Stella Gonçalves <135771572+stellag2003@users.noreply.github.com>
2023-06-16 21:02:58 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -56,6 +56,17 @@ namespace Aula08ListasERepeticao
                 contador = contador + 1;
             }
 
+            // Exemplo
+//while ( 1 > 0 )
+//{
+//Console.WriteLine("Olá");
+//}
+//Nesse exemplo, enquanto o número 1 for menor do que 0, ele irá repetir a mensagem Olá. Como 1 sempre será
+//maior que zero, esse loop nunca vai terminar, ou só terminará quando o computador travar. Criamos um loop
+//infinito. 
+    
+        
+
             // Para acumular valores em uma variável
             contador = contador + 1;// Repete o valor que tinha antes + o novo valor
             contador += 1; // acrescenta um valor na variável que já existe
@@ -66,6 +77,22 @@ namespace Aula08ListasERepeticao
             contador /= 2;
             contador -= 2;
             contador *= 2;
+
+    //*Lógica de loops aplicado a listas
+//List<string> nomes = new List<string>();
+//nomes.Add("Camila");
+//nomes.Add("Stella");
+//nomes.Add("William");
+//int contador = 0;
+//while ( contador < nomes.Count )
+//{
+//Console.WriteLine($"O nome é {nomes[contador]} ");
+//contador++;
+//}
+//No exemplo acima, o loop vai continuar enquanto o número de itens dentro da lista nomes for menor do que o
+//contador. O contador aumenta de um em um. E no WriteLine, temos o contador como número do índice a se
+//repetir.
+            
 
 
 
@@ -165,12 +192,12 @@ namespace Aula08ListasERepeticao
 
             Tanto um como o outro servem para guardar um conjunto de dados dentro de uma mesma variável C#.
 
-            Os Arrays são limitados. É preciso definir um tamanho limite ao criar um novo array, eemplo:
+            Os Arrays são limitados. É preciso definir um tamanho limite ao criar um novo array, exemplo:
 
             */
-            string[] nomes1 = new string[3];
+            string[] nomes1 = new string[3]; // cria e define um tamanho
             //OU
-            int[] idades = { 1, 2, 3, 4, 5 };
+            int[] idades = { 1, 2, 3, 4, 5 }; // cria com tamanho automatico
 
             /* 
              * As listas funcionam como os arrays, a diferença é que uma lista não tem um tamanho definido e pode 
@@ -178,13 +205,15 @@ namespace Aula08ListasERepeticao
              * 
              */
 
-            List<string> nomes2 = new List<string>();
+            List<string> nomes2 = new List<string>(); // atenção em como criar uma lista;
 
+            //Comandos uteis da lista
+             
+            nomes.Add("Conrado"); // Add -> adiciona um novo item
+            nomes.Remove("Harry"); // Remove -> remove um item da lista pelo valor dele
+            nomes.Add(2); // RemoveAt -> remove um item da lista pela posição
 
-            nomes.Add("Conrado");
-            nomes.Add("Harry");
-            nomes.Add("Zayn");
-
+            // tanto arrays como listas começam sua contagem do 0
 
 
 
